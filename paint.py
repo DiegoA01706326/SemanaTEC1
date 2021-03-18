aca67982b44c254be68570bbc2174e4c2244bb3f
unknown <A01706326@itesm.mx>
2021-03-17 20:18:50 -0600

    Color nuevo y funcion circulo

--- paint.py
+++ paint.py
@@ -33,9 +33,21 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
+def circulo(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+
+    circle(end.x - start.x)
+        
+
+    end_fill()
+
+
+
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -71,9 +83,10 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('pink'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', circulo), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
